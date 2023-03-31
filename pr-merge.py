960d683f7291eeb81ea80df2892fb20bba8e9806
Govardhana M K <mk@mahina.dev>
2023-03-31 14:55:12 +0530

    dry run: MAP-1

--- pr-merge.py
+++ pr-merge.py
@@ -28,7 +28,7 @@ payload = {"assignees": [github_username]}
 # Make a GET request to check if the PR is already assigned to the specified user
 get_response = requests.get(url, headers=headers)
 if get_response.status_code != 200:
-    print("Error getting PR information:", get_response.json())
+    print("Error getting PR information:", get_response.text)
 else:
     assignees = [assignee['login'] for assignee in get_response.json()['assignees']]
     print("Current assignees:", assignees)
@@ -43,6 +43,6 @@ else:
         # Make a POST request to assign the user to the PR
         post_response = requests.post(url, headers=headers, data=json.dumps(payload))
         if post_response.status_code != 201:
-            print(f"Error assigning {github_username} to PR #{pr_number}:", post_response.json())
+            print(f"Error assigning {github_username} to PR #{pr_number}:", post_response.text)
         else:
             print(f"{github_username} has been assigned to PR #{pr_number}")
