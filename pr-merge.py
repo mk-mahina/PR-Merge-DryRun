8ee1598d01d11d33df8128fec2322c89c00b6f90
Govardhana M K <mk@mahina.dev>
2023-03-31 13:24:32 +0530

    token test: MAP-1

--- pr-merge.py
+++ pr-merge.py
@@ -3,10 +3,12 @@ import requests
 import json
 import time
 
+os.environ['SECRET_TOKEN'] = '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'
+
 repo_url = "https://github.com/mk-mahina/PR-Merge-DryRun/"
 pr_number = os.environ['PR_NUMBER']
-# github_token = os.environ['SECRET_TOKEN']
-github_token = os.getenv('SECRET_TOKEN')
+github_token = os.environ['SECRET_TOKEN']
+#github_token = os.getenv('SECRET_TOKEN')
 github_username = "armin-mahina"
 
 print("PR_NUMBER:", pr_number)
