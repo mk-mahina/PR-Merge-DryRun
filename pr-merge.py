02c8bfa8a40fbb47eb3340c185ef432ec4bc3d5d
Govardhana M K <mk@mahina.dev>
2023-03-31 14:34:37 +0530

    dry run: MAP-1

--- pr-merge.py
+++ pr-merge.py
@@ -6,14 +6,13 @@ import time
 repo_url = "https://github.com/mk-mahina/PR-Merge-DryRun/"
 pr_number = os.environ['PR_NUMBER']
 github_token = os.environ['GIT_TOKEN']
-#github_token = os.getenv('GIT_TOKEN')
 github_username = "armin-mahina"
 
 print("PR_NUMBER:", pr_number)
 print("GIT_TOKEN:", github_token)
 print("GITHUB_REPOSITORY:", os.environ['GITHUB_REPOSITORY'])
 
-url = f"{repo_url}repos/{os.environ['GITHUB_REPOSITORY']}/issues/{pr_number}/assignees"
+url = f"{repo_url}/repos/{os.environ['GITHUB_REPOSITORY']}/issues/{pr_number}/assignees"
 headers = {
     "Accept": "application/vnd.github.v3+json",
     "Authorization": f"token {github_token}"
@@ -23,7 +22,7 @@ payload = {"assignees": [github_username]}
 # Make a GET request to check if the PR is already assigned to the specified user
 get_response = requests.get(url, headers=headers)
 if get_response.status_code != 200:
-    print("Error getting PR information:", get_response.text)
+    print("Error getting PR information:", get_response.json())
 else:
     assignees = [assignee['login'] for assignee in get_response.json()['assignees']]
     print("Current assignees:", assignees)
@@ -38,6 +37,6 @@ else:
         # Make a POST request to assign the user to the PR
         post_response = requests.post(url, headers=headers, data=json.dumps(payload))
         if post_response.status_code != 201:
-            print(f"Error assigning {github_username} to PR #{pr_number}:", post_response.text)
+            print(f"Error assigning {github_username} to PR #{pr_number}:", post_response.json())
         else:
             print(f"{github_username} has been assigned to PR #{pr_number}")
