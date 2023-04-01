8fe6237c50b5c3306581b244db47fac0ba98cb40
Govardhana M K <mk@mahina.dev>
2023-04-01 17:45:38 +0530

    dry run: MAP-1

--- pr-merge.py
+++ pr-merge.py
@@ -4,16 +4,12 @@ import requests
 # Get environment variables
 token = os.environ['SECRET_TOKEN']
 repository = "mk-mahina/PR-Merge-DryRun"
-issue_number = '6'
-assignee = 'armin-mahina'
+pull_number = os.environ['PULL_NUMBER']
 
-# Set API URL
-url = f"https://api.github.com/repos/{repository}/issues/{issue_number}/assignees"
-
-# Set payload
-payload = {
-    "assignees": [assignee]
-}
+# Set API URLs
+pr_url = f"https://api.github.com/repos/{repository}/pulls/{pull_number}"
+reviews_url = f"{pr_url}/reviews"
+assignees_url = f"{pr_url}/assignees"
 
 # Set headers
 headers = {
@@ -21,8 +17,29 @@ headers = {
     "Authorization": f"token {token}"
 }
 
-# Send POST request
-response = requests.post(url, headers=headers, json=payload)
+# Check if the pull request is approved
+pr_response = requests.get(pr_url, headers=headers)
+pr_data = pr_response.json()
+reviews_response = requests.get(reviews_url, headers=headers)
+reviews_data = reviews_response.json()
+approvals = [review for review in reviews_data if review["state"] == "APPROVED"]
+if not approvals:
+    print("No approvals found. PR not assigned.")
+    exit(0)
 
-# Print response
-print(response.text)
+# Check if the pull request is already assigned
+assignees_response = requests.get(assignees_url, headers=headers)
+assignees_data = assignees_response.json()
+if any(assignee["login"] == "armin-mahina" for assignee in assignees_data):
+    print("PR is already assigned to armin-mahina.")
+    exit(0)
+
+# Assign the pull request to "armin-mahina"
+assignees_payload = {
+    "assignees": ["armin-mahina"]
+}
+assignees_response = requests.post(assignees_url, headers=headers, json=assignees_payload)
+if assignees_response.ok:
+    print("PR assigned to armin-mahina.")
+else:
+    print(f"Failed to assign PR to armin-mahina. Response: {assignees_response.text}")
