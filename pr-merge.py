cb699b6d65f15f6fb6bca5f32e4295470dcac917
Govardhana M K <mk@mahina.dev>
2023-04-01 19:04:11 +0530

    dry run: MAP-1

--- pr-merge.py
+++ pr-merge.py
@@ -26,21 +26,29 @@ if pr_data["state"] != "open":
 reviews_response = requests.get(reviews_url, headers=headers)
 reviews_data = reviews_response.json()
 approvals = [review for review in reviews_data if review["state"] == "APPROVED"]
-if not approvals:
+if len(approvals) < 1:
     print("No approvals found. PR not assigned.")
     exit(0)
 
-# Assign the pull request to "armin-mahina" and remove existing assignees
+# Remove existing assignees
+remove_assignees_url = f"https://api.github.com/repos/{repository}/issues/{pull_number}/assignees"
+existing_assignees_response = requests.get(remove_assignees_url, headers=headers)
+existing_assignees_data = existing_assignees_response.json()
+existing_assignees = existing_assignees_data["assignees"]
+for existing_assignee in existing_assignees:
+    remove_assignee_url = f"https://api.github.com/repos/{repository}/issues/{pull_number}/assignees/{existing_assignee['login']}"
+    remove_assignee_response = requests.delete(remove_assignee_url, headers=headers)
+    if remove_assignee_response.ok:
+        print(f"Removed existing assignee {existing_assignee['login']}.")
+    else:
+        print(f"Failed to remove existing assignee {existing_assignee['login']}. Response: {remove_assignee_response.text}")
 
-        print(pr.assignee.login)
-
-        pr.remove_from_assignees(pr.assignee.login)
-
-        # assign the pull request to a user for manual approval
-        pr.add_to_assignees("armin-mahina")
-
-        pr.update()  # update the pr object with the new assignee
-
-        print(pr.assignee.login)
-
-
+# Assign the pull request to "armin-mahina"
+assignees_payload = {
+    "assignees": ["armin-mahina"]
+}
+assignees_response = requests.post(assignees_url, headers=headers, json=assignees_payload)
+if assignees_response.ok:
+    print("PR assigned to armin-mahina.")
+else:
+    print(f"Failed to assign PR to armin-mahina. Response: {assignees_response.text}")
