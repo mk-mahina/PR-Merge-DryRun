850d25973826a90c7ae7d1ef453b1c61c42be3a6
Govardhana M K <mk@mahina.dev>
2023-04-02 07:26:40 +0530

    dry run: MAP-1

--- pr-merge.py
+++ pr-merge.py
@@ -8,7 +8,7 @@ pull_number = os.environ['PULL_NUMBER']
 
 # Set API URLs
 pr_url = f"https://api.github.com/repos/{repository}/pulls/{pull_number}"
-reviews_url = f"{pr_url}/reviews"
+reviews_url = f"{pr_url}/requested_reviewers"
 
 # Set headers
 headers = {
@@ -23,17 +23,13 @@ if pr_data["state"] != "open":
     print("PR is not open. Exiting.")
     exit(0)
 
-# Check if armin-mahina has reviewed the pull request
+# Check if armin-mahina has been added as a reviewer to the pull request
 reviews_response = requests.get(reviews_url, headers=headers)
 reviews_data = reviews_response.json()
-reviewed_by_armin = False
-for review in reviews_data:
-    if review["user"]["login"] == "armin-mahina":
-        reviewed_by_armin = True
-        break
-
-# Add armin-mahina as a reviewer if they haven't reviewed the pull request
-if not reviewed_by_armin:
+reviewers = [reviewer["login"] for reviewer in reviews_data["users"]]
+if "armin-mahina" in reviewers:
+    print("armin-mahina has already been added as a reviewer to the pull request.")
+else:
     reviewers_payload = {
         "reviewers": ["armin-mahina"]
     }
@@ -42,5 +38,3 @@ if not reviewed_by_armin:
         print("armin-mahina added as a reviewer to the pull request.")
     else:
         print(f"Failed to add armin-mahina as a reviewer to the pull request. Response: {reviewers_response.text}")
-else:
-    print("armin-mahina has already reviewed the pull request.")
