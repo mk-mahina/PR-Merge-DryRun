13f54ff96e57d8844525aca32072f833130b9c7b
Govardhana M K <mk@mahina.dev>
2023-04-02 08:42:32 +0530

    dry run: MAP-1

--- pr-merge.py
+++ pr-merge.py
@@ -3,9 +3,9 @@ import requests
 
 # Get environment variables
 token = os.environ['SECRET_TOKEN']
-repository = os.environ['GITHUB_REPOSITORY']
+repository = "mk-mahina/PR-Merge-DryRun"
 pull_number = os.environ['PULL_NUMBER']
-review_gate_name = os.environ['REVIEW_GATE_NAME']
+review_gate_name = "review-gate"
 
 # Set API URLs
 pr_url = f"https://api.github.com/repos/{repository}/pulls/{pull_number}"
@@ -29,7 +29,7 @@ if pr_data["state"] != "open":
 reviewers_response = requests.get(reviews_url, headers=headers)
 reviewers_data = reviewers_response.json()
 
-armin_reviewer = next((reviewer for reviewer in reviewers_data if reviewer['user']['login'] == 'armin-mahina'), None)
+armin_reviewer = next((reviewer for reviewer in reviewers_data["reviews"] if reviewer['user']['login'] == 'armin-mahina'), None)
 if armin_reviewer is None:
     review_payload = {
         "body": "Please review this PR",
@@ -49,30 +49,25 @@ statuses_response = requests.get(statuses_url, headers=headers)
 statuses_data = statuses_response.json()
 
 if "statuses" not in statuses_data:
-    print(f"Error: statuses not found in response: {statuses_data}")
-    exit(1)
-
-review_gate_statuses = [status for status in statuses_data["statuses"] if status["context"] == review_gate_name]
-
-if review_gate_statuses:
-    print(f"{review_gate_name} status check already exists.")
-else:
-    # Check if another reviewer has approved the PR
-    reviews_response = requests.get(reviews_url, headers=headers)
-    reviews_data = reviews_response.json()
-
-    approved_reviewers = [review['user']['login'] for review in reviews_data if review['state'] == 'APPROVED' and review['user']['login'] != 'armin-mahina']
-
-    if approved_reviewers:
-        print(f"{approved_reviewers[0]} has already approved the PR. Skipping {review_gate_name} status check.")
+    # Create the status check if it does not exist yet
+    status_payload = {
+        "state": "pending",
+        "description": f"This PR is waiting for manual approval from armin-mahina before merging. ({review_gate_name})",
+        "context": review_gate_name
+    }
+    status_response = requests.post(statuses_url, headers=headers, json=status_payload)
+    if status_response.ok:
+        print(f"{review_gate_name} status check created.")
     else:
-        status_payload = {
-            "state": "pending",
-            "description": f"This PR is waiting for manual approval from armin-mahina before merging. ({review_gate_name})",
-            "context": review_gate_name
-        }
-        status_response = requests.post(statuses_url, headers=headers, json=status_payload)
-        if status_response.ok:
-            print(f"{review_gate_name} status check created.")
-        else:
-            print(f"Failed to create {review_gate_name} status check. Response: {status_response.text}")
+        print(f"Failed to create {review_gate_name} status check. Response: {status_response.text}")
+else:
+    review_gate_statuses = [status for status in statuses_data["statuses"] if status["context"] == review_gate_name]
+
+    if review_gate_statuses:
+        print(f"{review_gate_name} status check already exists.")
+    else:
+        # Check if another reviewer has approved the PR
+        reviews_response = requests.get(reviews_url, headers=headers)
+        reviews_data = reviews_response.json()
+
+        approved_review
