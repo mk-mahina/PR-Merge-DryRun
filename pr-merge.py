9bf3576a365a47a8bf570ea1c82d5f482750c830
Govardhana M K <mk@mahina.dev>
2023-04-02 07:35:45 +0530

    dry run: MAP-1

--- pr-merge.py
+++ pr-merge.py
@@ -35,8 +35,27 @@ else:
         "description": "This PR is waiting for manual approval",
         "context": "Waiting for Manual Approval"
     }
-    status_response = requests.post(statuses_url, headers=headers, json=status_payload)
-    if status_response.ok:
+    create_status_response = requests.post(statuses_url, headers=headers, json=status_payload)
+    if create_status_response.ok:
         print("Waiting for Manual Approval status check created.")
     else:
-        print(f"Failed to create Waiting for Manual Approval status check. Response: {status_response.text}")
+        print(f"Failed to create Waiting for Manual Approval status check. Response: {create_status_response.text}")
+        exit(1)
+
+# Check if "armin-mahina" has been added as a reviewer
+reviewers_url = f"{pr_url}/requested_reviewers"
+reviewers_response = requests.get(reviewers_url, headers=headers)
+reviewers_data = reviewers_response.json()
+if "armin-mahina" in [reviewer["login"] for reviewer in reviewers_data["users"]]:
+    print("armin-mahina has been added as a reviewer.")
+else:
+    add_reviewer_url = f"{pr_url}/requested_reviewers"
+    add_reviewer_payload = {
+        "reviewers": ["armin-mahina"]
+    }
+    add_reviewer_response = requests.post(add_reviewer_url, headers=headers, json=add_reviewer_payload)
+    if add_reviewer_response.ok:
+        print("armin-mahina has been added as a reviewer.")
+    else:
+        print(f"Failed to add armin-mahina as a reviewer. Response: {add_reviewer_response.text}")
+        exit(1)
